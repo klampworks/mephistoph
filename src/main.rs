9753548512f24ebbfd98c26995fcfcf4643c67b9
yukipyon <wolfvbear@gmail.com>
2015-07-10 01:13:59 +0000

    Added xor function.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::io::Read;
 
+fn xor(key: u8, data: u8) -> u8 {
+    key ^ data
+}
+
 fn main() {
     let mut buf = [0u8, 1];
 
     while std::io::stdin().read(&mut buf)
         .ok()
         .expect("a") != 0 {
-
-        print!("{}", buf[0])
+        print!("{}", xor(7, buf[0]))
 
     }
     println!("Hello, world!");
