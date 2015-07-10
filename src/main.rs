eb8ab085c32a6150cd574beb08cc405fb02b0c82
yukipyon <wolfvbear@gmail.com>
2015-07-10 02:57:50 +0000

    Implemented buffered xor.

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,25 @@ fn xor(key: u8, data: u8) -> u8 {
     key ^ data
 }
 
+fn xor_buf(key: u8, buf: &mut [u8]) {
+    for x in buf.iter_mut() {
+        *x = xor(key, *x)
+    }
+}
+
 fn main() {
-    let mut buf = [0u8, 1];
+    let mut buf = [0u8, 10];
 
     while std::io::stdin().read(&mut buf)
         .ok()
         .expect("a") != 0 {
-        print!("{}", xor(7, buf[0]))
+
+        xor_buf(7, &mut buf);
+
+        for x in buf.iter() {
+            print!("{}", x)
+        }
 
     }
-    println!("Hello, world!");
+    //println!("Hello, world!");
 }
