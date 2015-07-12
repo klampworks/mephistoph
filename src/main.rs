cd80e3d27ee229c8777d97efce4a02a0195df9aa
yukipyon <wolfvbear@gmail.com>
2015-07-12 15:08:16 +0000

    Implemented rolling xor with a [u8] key.

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,28 @@ fn xor(key: u8, data: u8) -> u8 {
     key ^ data
 }
 
-fn xor_buf(key: u8, buf: &mut [u8]) {
+fn xor_buf(key: &[u8], mut key_i: usize, buf: &mut [u8]) -> usize{
+
+    let len = buf.len();
+
     for x in buf.iter_mut() {
-        *x = xor(key, *x)
+        *x = xor(key[key_i], *x);
+        key_i = ((key_i + 1) % len) - 1;
     }
+
+    return key_i;
 }
 
 fn main() {
-    let mut buf = [0u8, 10];
+    let mut buf = [0u8; 1];
+    let key : [u8; 4] = [1, 2, 3, 4];
+    let mut key_i = 0;
 
     while std::io::stdin().read(&mut buf)
         .ok()
         .expect("Could not read from stdin.") != 0 {
 
-        xor_buf(7, &mut buf);
+        key_i = xor_buf(&key, key_i, &mut buf);
 
         std::io::stdout().write(&buf)
             .ok()
