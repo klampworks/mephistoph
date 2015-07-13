58397813c3f192450874bf5485a8c4956679e467
yukipyon <wolfvbear@gmail.com>
2015-07-13 18:18:42 +0000

    Added xor unit tests.

--- src/main.rs
+++ src/main.rs
@@ -40,3 +40,13 @@ fn main() {
     }
     //println!("Hello, world!");
 }
+
+#[test]
+fn test_xor_val() {
+    assert_eq!(1, xor(6, 7))
+}
+
+#[test]
+fn test_xor_id() {
+    assert_eq!(7, xor(6, xor(6, 7)))
+}
