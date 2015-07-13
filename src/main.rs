80a7e61f9918002f484455e90e205819b087ee68
yukipyon <wolfvbear@gmail.com>
2015-07-13 18:53:50 +0000

    Unit tests for sliding xor.

--- src/main.rs
+++ src/main.rs
@@ -50,3 +50,43 @@ fn test_xor_val() {
 fn test_xor_id() {
     assert_eq!(7, xor(6, xor(6, 7)))
 }
+
+#[test]
+fn test_xor_buf_1k_1b() {
+    let mut buf = [7u8];
+
+    // New index for a length 1 key should be 0.
+    assert_eq!(0, xor_buf(&[6], 0, &mut buf));
+    assert_eq!(1, buf[0])
+}
+
+#[test]
+fn test_xor_buf_5k_1b() {
+    let mut buf = [7u8];
+    let mut ki = 0;
+    let key = [1,2,3,4,5];
+    let b_exp = [6, 4, 7, 3, 6];
+    let k_exp = [1,2,3,4,0];
+
+    for i in (0..b_exp.len()) {
+        ki = xor_buf(&key, ki, &mut buf);
+        assert_eq!(k_exp[i], ki);
+        assert_eq!(b_exp[i], buf[0])
+    }
+}
+
+#[test]
+fn test_xor_buf_1k_5b() {
+    let mut buf = [1,2,3,4,5];
+    let mut ki = 0;
+    let key = [6];
+    let b_exp = [7, 4, 5, 2, 3];
+    let k_exp = 0;
+
+    ki = xor_buf(&key, ki, &mut buf);
+    assert_eq!(k_exp, ki);
+
+    for i in (0..b_exp.len()) {
+        assert_eq!(b_exp[i], buf[i])
+    }
+}
